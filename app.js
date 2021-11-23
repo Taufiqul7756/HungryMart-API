29e922e407ef3b89189690783c506e8354c3ce78
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-11-23 23:57:55 +0600

    Meal's API access

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => displayMeal(data));
 
-//   const displayMeal = meals =>{
+const displayMeal = (meals) => {
+  const mealsDetails = document.getElementById("meals");
+  for (let i = 0; i < meals.length; i++) {
+    const userMenu = meals[i];
 
-//   }
+    const mealDiv = document.createElement("div");
+    const h3 = document.createElement("h3");
+
+    mealClass.className = "strMeal";
+    const mealsInfo = `
+    <h3 class ="mealName" > ${meal.name.common}</h3>
+    
+    `;
+
+    countryDiv.innerHTML = countryInfo;
+
+    countriesDiv.appendChild(countryDiv);
+  }
+};
+
+{
+  /* <p class="capitalName" > Capital: ${country.capital} </p>
+    <p class="regionName" > Region: ${country.region} </p>
+    <button onclick="displayCountry('${country.name.common}')">Details </button> */
+}
